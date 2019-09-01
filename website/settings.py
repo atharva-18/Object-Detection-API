dd054b23a9fe37236a1fe92ed1cfdf58a2f91769
AtharvaPusalkar <atharvapusalkar18@gmail.com>
2019-09-01 19:10:50 +0530

    Made changes for hosting

--- website/settings.py
+++ website/settings.py
@@ -79,9 +79,10 @@ WSGI_APPLICATION = 'website.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 
